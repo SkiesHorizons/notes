e279b694d7c681a4a17094484ae0751437d89f25
Nguyễn Thanh Tân <me@tozydev.id.vn>
2025-07-26 15:47:09 +0700

    feat: add folder creation and update functionality with notifications

--- app/src/components/folder-edit-modal/folder-edit-modal.tsx
+++ app/src/components/folder-edit-modal/folder-edit-modal.tsx
@@ -4,12 +4,13 @@ import { useStore } from "@tanstack/react-store"
 import { Form, useForm } from "@mantine/form"
 import { z } from "zod"
 import { zod4Resolver } from "mantine-form-zod-resolver"
-import { useQuery } from "@tanstack/react-query"
-import { queries } from "@/lib/queries"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { mutations, queries } from "@/lib/queries"
 import { useEffect, useMemo } from "react"
+import { notifications } from "@mantine/notifications"
 
 const zFolderFormValues = z.object({
-  name: z.string().min(1, "Folder name is required"),
+  name: z.string().nonempty("Folder name is required"),
   parentId: z.uuid().nullable(),
 })
 
@@ -29,7 +30,8 @@ export function FolderEditModal() {
       parentId: initialFolder ? initialFolder.parentId : initialParentFolderId || null,
     })
     form.reset()
-  }, [form, initialFolder, initialParentFolderId])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [initialFolder, initialParentFolderId])
 
   const folderOptions = useMemo(() => {
     return (
@@ -40,7 +42,60 @@ export function FolderEditModal() {
     )
   }, [folders])
 
+  const queryClient = useQueryClient()
+  const { mutate: createFolder } = useMutation({
+    ...mutations.folders.create(),
+    onSuccess: async (created) => {
+      await queryClient.invalidateQueries({
+        queryKey: queries.folders.list().queryKey,
+      })
+      await queryClient.invalidateQueries({
+        queryKey: queries.folders.list({ parentId: created.parentId }).queryKey,
+      })
+    },
+    onError: (error) => {
+      notifications.show({
+        color: "red",
+        title: "Error creating folder",
+        message: error.message || "An error occurred while creating the folder.",
+      })
+    },
+  })
+
+  const { mutate: patchFolder } = useMutation({
+    ...mutations.folders.patch(),
+    onSuccess: async (updated) => {
+      await queryClient.setQueryData(queries.folders.list().queryKey, (old: any[]) =>
+        old.map((folder) => (folder.id === updated.id ? updated : folder)),
+      )
+    },
+    onError: (error) => {
+      notifications.show({
+        color: "red",
+        title: "Error updating folder",
+        message: error.message || "An error occurred while updating the folder.",
+      })
+    },
+  })
+
   const handleSubmit = (values: Partial<z.infer<typeof zFolderFormValues>>) => {
+    if (initialFolder) {
+      const data = {
+        name: values.name === initialFolder.name ? undefined : values.name,
+        parentId: values.parentId === initialFolder.parentId ? undefined : values.parentId,
+      }
+      if (Object.keys(data).length !== 0) {
+        patchFolder({
+          folderId: initialFolder.id,
+          data,
+        })
+      }
+    } else if (values.name) {
+      createFolder({
+        name: values.name,
+        parentId: values.parentId || null,
+      })
+    }
     folderEditModal.close()
     form.reset()
   }
